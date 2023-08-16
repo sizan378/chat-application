ed632d18c342231f3e90de42fd69b64ada207317
sizan378 <sizan378>
2023-08-16 16:25:10 +0600

    ✨ feat(jwt setup): jwt token setup
    after login user get jwt token

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,3 +1,10 @@
+// External Import
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+// internal imports
+const User = require("../models/person")
+
 // get login page
 function getLogin(req, res, next) {
     res.render("index", {
@@ -6,4 +13,32 @@ function getLogin(req, res, next) {
     
 }
 
-module.exports = {getLogin}
+async function userLogin(req, res) {
+    email = req.body.email
+    password = req.body.password
+
+    const user = await User.findOne({email})
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        // Create token
+        const token = jwt.sign(
+          { user_id: user._id, email, role:user.role },
+          process.env.TOKEN_KEY,
+          {
+            expiresIn: "2h",
+          }
+        );
+
+        // save user token
+        user.token = token;
+
+        // user
+        res.status(200).json(user);
+    } else {
+        res.status(400).json({
+            message: "Invalid Password"
+        })
+    }
+}
+
+module.exports = {getLogin, userLogin}
